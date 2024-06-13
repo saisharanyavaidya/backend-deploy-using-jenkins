db92e450c5b30737d5b35160d71fc4d59f10ddc0
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-06-14 00:22:06 +0530

    nexus

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -4,4 +4,4 @@ environment=$2
 app_version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/saisharanyavaidya/ansible-expense-roles-jenkins.git main.yaml -e component=$component -e env=$environment appVersion=$app_version
+ansible-pull -i localhost, -U https://github.com/saisharanyavaidya/ansible-expense-roles-jenkins.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_version
